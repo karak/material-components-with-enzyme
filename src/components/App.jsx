c97970eb67697d74754b8af4e3cbae6e69946b82
karak <karak97@gmail.com>
2018-02-23 15:11:06 +0900

    Replaced initialization.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { autoInit } from 'material-components-web';
+import { MDCTextField } from '@material/textfield';
 
 export default class App extends React.PureComponent {
   render() {
     return (
       <div className="mdc-typography">
         <h2 className="mdc-typography--display2">Hello, Material Components!</h2>
-        <div className="mdc-text-field" data-mdc-auto-init="MDCTextField">
+        <div className="mdc-text-field" ref={(input) => this.root = input}>
             <input type="text" className="mdc-text-field__input" id="demo-input"/>
             <label htmlFor="demo-input" className="mdc-text-field__label">Tell us how you feel!</label>
         </div>
@@ -16,6 +16,11 @@ export default class App extends React.PureComponent {
   }
 
   componentDidMount() {
-    autoInit();
+    this.component = MDCTextField.attachTo(this.root);
+  }
+
+  componentWillUnmount() {
+    this.component.destroy();
+    this.component = null;
   }
 }
--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom';
 import { Simulate } from 'react-test-utils';
 */
 import '../../setup-enzyme';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import App from './App';
 
 describe('<App/>', () => {
   it('has header', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = mount(<App />);
 
     expect(wrapper.find('.mdc-typography .mdc-typography--display2').text()).toBe('Hello, Material Components!');
   });
@@ -30,11 +30,10 @@ describe('<App/>', () => {
       document.body.removeChild(attachedElement);
     });
 
-    it('auto-initialized', () => {
+    it('is initialized', () => {
       const wrapper = mount(<App />, { attachTo: attachedElement });
 
       try {
-        expect(wrapper.find('.mdc-text-field').getDOMNode().getAttribute('data-mdc-auto-init')).toBe('MDCTextField');
         expect(hasDomClass.call(wrapper.find('.mdc-text-field'), 'mdc-text-field--upgraded')).toBeTruthy();
       } finally {
         wrapper.detach();
@@ -45,23 +44,9 @@ describe('<App/>', () => {
       const wrapper = mount(<App />, { attachTo: attachedElement });
 
       try {
-        const clickEvent = document.createEvent('MouseEvent');
-        clickEvent.initEvent('click', true, true);
-        wrapper.find('.mdc-text-field').getDOMNode().dispatchEvent(clickEvent);
-        /*
-        wrapper.find('.mdc-text-field').getDOMNode().click();
-        */
-        /*
-        const field = wrapper.find('.mdc-text-field').getDOMNode();
-        const clickEvent = document.createEvent('MouseEvent');
-        clickEvent.initMouseEvent('click', true, true, window, undefined, 0, 0, 0, 0, false, false, false, false, 0, field);
-        field.dispatchEvent(clickEvent);
-        */
-        /*
-        Simulate.click(wrapper.find('.mdc-text-field').getDOMNode());
-        */
-        console.info(attachedElement.innerHTML);
-        //wrapper.update();
+        wrapper.find('.mdc-text-field__input').getDOMNode().focus();
+        // "TypeError: Cannot read property 'valid' of undefined",
+        // as 'jsdom' has no form validation: https://github.com/jsdom/jsdom/issues/544
         expect(hasDomClass.call(wrapper.find('.mdc-text-field'), 'mdc-text-field--focused')).toBeTruthy();
       } finally {
         wrapper.detach();
